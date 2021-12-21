18fc5906ea07f3caa40f4de4791badfd8d030e6a
trieunv <ngvtrieusqtt@gmail.com>
2021-12-21 07:20:17 +0700

    t

---
+++ 12-SourceCode/HT/QRMS/QRMS/QRMS/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using QRMS.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace QRMS.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
