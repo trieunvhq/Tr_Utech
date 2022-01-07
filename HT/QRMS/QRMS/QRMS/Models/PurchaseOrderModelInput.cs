9dcacec27aed80157327c3707ca60eb124db616d
thinmta <31883027+thinmta@users.noreply.github.com>
2022-01-07 09:38:05 +0700

    a

---
+++ HT/QRMS/QRMS/QRMS/Models/PurchaseOrderModelInput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace QRMS.Models
+{
+    public class PurchaseOrderModelInput
+    {
+        public string WarehouseCode { get; set; }
+        public DateTime from_day { get; set; }
+        public DateTime to_day { get; set; }
+    }
+}
---
+++ HT/QRMS/QRMS/QRMS/Models/WarehouseBPLModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace QRMS.Models
+{
+
+    public class WarehouseBPLModel
+    {
+        public string ID { get; set; }
+        public string WarehouseCode { get; set; }
+        public string WarehouesName { get; set; }
+    }
+}
