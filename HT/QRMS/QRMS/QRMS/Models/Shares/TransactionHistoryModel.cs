2436fca299233b41598c2fa78a3d9f344c93daaa
trieunv <ngvtrieusqtt@gmail.com>
2022-01-09 22:51:49 +0700

    t

--- HT/QRMS/QRMS/QRMS/Models/Shares/TransactionHistoryModel.cs
+++ HT/QRMS/QRMS/QRMS/Models/Shares/TransactionHistoryModel.cs
@@ -7,8 +7,6 @@ namespace QRMS.Models
     public class TransactionHistoryModel
     {
         public long ID { get; set; }
-        public int page { get; set; }
-        public string token { get; set; }
         public string TransactionType { get; set; }
         public string OrderNo { get; set; }
         public Nullable<System.DateTime> OrderDate { get; set; }
@@ -39,11 +37,13 @@ namespace QRMS.Models
         public string CustomerName { get; set; }
         public string SupplierCode { get; set; }
         public string SupplierName { get; set; }
+        public string ExportStatus { get; set; }
         public string RecordStatus { get; set; }
         public Nullable<System.DateTime> CreateDate { get; set; }
         public string UserCreate { get; set; }
         public Nullable<System.DateTime> UpdateDate { get; set; }
         public string UserUpdate { get; set; }
-
+        public int page { get; set; }
+        public string token { get; set; }
     }
 }
