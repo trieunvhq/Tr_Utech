891e9e6ab8d477dfb1b7eee02d79fba09aaf1a46
thinmta <31883027+thinmta@users.noreply.github.com>
2022-01-18 17:05:15 +0700

    a

---
+++ HT/QRMS/QRMS/QRMS/Views/NKDC/ChonPhieuNhapPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace QRMS.Views.NKDC
+{
+    public partial class ChonPhieuNhapPage : ContentPage
+    {
+        public ChonPhieuNhapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
